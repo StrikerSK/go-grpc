267efc46077a22c6b31eb96145c2054814ee676f
Teh|Striker <kristianhanus@gmail.com>
2022-10-02 15:52:50 +0200

    Splitting codebase - Todo Commons

---
+++ commons/todo/domain/TodoEntity.go
@@ -0,0 +1,31 @@
+package domain
+
+import "github.com/StrikerSK/go-grpc/proto/todo"
+
+type TodoStructure struct {
+	Id          string   `bson:"id" json:"id"`
+	Name        string   `bson:"name" json:"name"`
+	Description string   `bson:"description" json:"description"`
+	Done        bool     `bson:"done" json:"done"`
+	Tags        []string `bson:"tags" json:"tags"`
+}
+
+func ConvertFromProto(input *todo.Todo) TodoStructure {
+	return TodoStructure{
+		Id:          input.Id,
+		Name:        input.Name,
+		Description: input.Description,
+		Done:        input.Done,
+		Tags:        input.Tags,
+	}
+}
+
+func (r *TodoStructure) ConvertToProto() *todo.Todo {
+	return &todo.Todo{
+		Id:          r.Id,
+		Name:        r.Name,
+		Description: r.Description,
+		Done:        r.Done,
+		Tags:        r.Tags,
+	}
+}
